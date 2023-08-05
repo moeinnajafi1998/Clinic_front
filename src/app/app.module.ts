539c5e72e230c31e3c2c4277279ea20ed44e802c
moein najafi <test@gmail.com>
2023-08-05 14:24:38 +0330

    section1 added => bg

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { Section1Component } from './components/section1/section1.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    Section1Component,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
