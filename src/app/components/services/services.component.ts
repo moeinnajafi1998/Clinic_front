0176b5c2530c50d8712d1538071b859c03def5a0
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-19 16:09:10 +0330

    services component

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Service } from 'src/app/models/Service';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-services',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./services.component.css']
 })
 export class ServicesComponent {
+  constructor(private http:HttpService,private router:Router){}
+  services : Service[] = [];
+  ngOnInit(){
+    this.http.services().pipe().subscribe(res=>{
+      this.services = res;
+    })
+  }
+
+  // serviceDetails(id:number){
+  //   this.router.navigate([`/service/${id}`]);
+  // }
 
 }
