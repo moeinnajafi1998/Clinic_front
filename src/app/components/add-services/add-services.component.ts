4ef4d6ed88662b37f3e69e02426be96df4a722ab
MoeinNajafi1998 <MoeinNajafi1996@gmail.com>
2023-08-19 16:20:10 +0330

    add service component

--- src/app/components/add-services/add-services.component.ts
+++ src/app/components/add-services/add-services.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpService } from 'src/app/services/http.service';
+declare var $ :any;
 
 @Component({
   selector: 'app-add-services',
@@ -6,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-services.component.css']
 })
 export class AddServicesComponent {
+  formData = new FormData();
+  constructor(private http:HttpService,private router:Router){}
+
+  ngOnInit(){}
+
+
+  createService(e:Event){
+    e.preventDefault();
+    this.formData.append('name',$('#name').val());
+    this.formData.append('description',$('#description').val());
+    this.http.createService(this.formData).pipe().subscribe(res=>{
+      this.router.navigate(['/services/']);
+    });
+  }
+
 
 }
